8acd67b256f26ae836de5a22c723215dca375c56
ARJUN AGARWAL <123585663+sahhoArjun097@users.noreply.github.com>
2025-02-08 03:14:10 +0530

    added vercel .json

--- src/config/FirebaseAuth.js
+++ src/config/FirebaseAuth.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-ZkN3wcBrgrYd_Laxx2Rffb-3s7WZDFs",
-  authDomain: "swiggi-auth.firebaseapp.com",
-  projectId: "swiggi-auth",
-  storageBucket: "swiggi-auth.appspot.com", 
-  messagingSenderId: "588294331418",
-  appId: "1:588294331418:web:cafc1ea94c3391135445a3",
-};
+    apiKey: "AIzaSyD-ZkN3wcBrgrYd_Laxx2Rffb-3s7WZDFs",
+    authDomain: "swiggi-auth.firebaseapp.com",
+    projectId: "swiggi-auth",
+    storageBucket: "swiggi-auth.firebasestorage.app",
+    messagingSenderId: "588294331418",
+    appId: "1:588294331418:web:cafc1ea94c3391135445a3"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
