037d7435dfae61f67f34b9dae82f6e719e12dc84
sahhoArjun097 <heonyagarwal@gmail.com>
2025-10-07 01:00:06 +0530

    added the env

--- src/config/FirebaseAuth.js
+++ src/config/FirebaseAuth.js
@@ -1,11 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-console.log(JSON.parse(import.meta.env.VITE_KEY));
-const firebaseConfig = JSON.parse(import.meta.env.VITE_KEY);
 
+// Get values from environment variables
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Auth and Provider
 const auth = getAuth(app);
-const provider = new GoogleAuthProvider(); 
+const provider = new GoogleAuthProvider();
 
 export { auth, provider };
